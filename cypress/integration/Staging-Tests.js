c9acc0fc9295d7c184963bb78310164af203c806
YazenHalid <yazan-kh@hotmail.com>
2019-12-17 14:42:37 +0300

    17-1-19 assertion

--- cypress/integration/Staging-Tests.js
+++ cypress/integration/Staging-Tests.js
@@ -1,11 +1,17 @@
 /// <reference types="Cypress" />
 
 context('Staging-Test', () => {
-    beforeEach(() => {
+    before(() => {
         cy.viewport('macbook-15')
         cy.visit('https://webcore.mintplatform.net')
         cy.Sign_in()
-    })
+         //count spaces//
+        cy.get('[class="btn btn-default"]').its('length').then(numSpace => {
+            let num = numSpace
+            cy.log('Number of Spaces: '+num)  
+          })
+        })
+    
 
     describe('test_All_Pages', () => {
 
@@ -28,6 +34,8 @@ context('Staging-Test', () => {
             cy.get('#open-button').click()//side-menu
             cy.get('[href="/Home/About"]').click()//About_Page
             cy.url().should('include', 'About')
+            cy.get('#open-button').click()//side-menu
+            cy.get('nav > [href="/"]').click()//Home_Page
             cy.get('#userDropDown').click({ force: true })
             cy.get('#userDropDown_1').click({ force: true })
             cy.url().should('include', 'Profile')
@@ -36,9 +44,47 @@ context('Staging-Test', () => {
     })
 
     describe('test_open_Space', () => {
-        it('open_Space', () => {
-            cy.get('[class="btn btn-default"]').first().click()
 
+        before(() => {
+            cy.viewport('macbook-15')
+            cy.visit('https://webcore.mintplatform.net')
+            cy.Sign_in()
+             //count spaces//
+            cy.get('[class="btn btn-default"]').its('length').then(numSpace => {
+                     let num = numSpace
+                     cy.log('Number of Spaces: '+num)  
+                   })
+            })
+        
+         it('Assert_content', () => {
+            cy.get('[class="btn btn-default"]').first().click()
+            cy.get('.active > .nav-link').should('contain', 'المحتوى التعليمي','content')
+            })
+        it('Assert_assessments', () => {
+            
+            cy.get(':nth-child(2) > .nav-link').should('contain', 'التدريبات','assessments')
+        })
+        it('Assert_inquiry', () => {
+            
+            cy.get(':nth-child(3) > .nav-link').should('contain', 'أسئلة الطلاب','inquiry')
+        })
+        it('Assert_challenge', () => {
+           
+            cy.get(':nth-child(4) > .nav-link').should('contain', 'التحديات','challenge')
+        })
+        it('Assert_community', () => {
+          
+            cy.get('#nav-item-community > .nav-link').should('contain', 'المجتمع','community')
+        })
+        it('Assert_report', () => {
+          
+            cy.get(':nth-child(6) > .nav-link').should('contain', 'تقارير‎','report')
+        })
+
+        it('Assert_about', () => {
+          
+            cy.get('#aAbout').should('contain', 'عن المساحة','about')
         })
     })
 })
+
